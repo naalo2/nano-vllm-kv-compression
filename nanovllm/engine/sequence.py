2853e949b894a39b0ad4cdf9ec1a132f39d6eb51
naalo2 <138016975+naalo2@users.noreply.github.com>
2026-05-08 15:27:01 +0800

    change sequence

--- nanovllm/engine/sequence.py
+++ nanovllm/engine/sequence.py
@@ -12,8 +12,12 @@ class SequenceStatus(Enum):
 
 
 class Sequence:
-    block_size = 256
+    block_size: int = 0  # invalid value, will be set by set_block_size
     counter = count()
+
+    @classmethod
+    def set_block_size(cls, block_size: int):
+        cls.block_size = block_size
 
     def __init__(self, token_ids: list[int], sampling_params = SamplingParams()):
         self.seq_id = next(Sequence.counter)
@@ -23,12 +27,11 @@ class Sequence:
         self.num_tokens = len(self.token_ids)
         self.num_prompt_tokens = len(token_ids)
         self.num_cached_tokens = 0
-        self.num_scheduled_tokens = 0
-        self.is_prefill = True
         self.block_table = []
         self.temperature = sampling_params.temperature
         self.max_tokens = sampling_params.max_tokens
         self.ignore_eos = sampling_params.ignore_eos
+        self.prefilled = False
 
     def __len__(self):
         return self.num_tokens
@@ -53,6 +56,10 @@ class Sequence:
         return self.token_ids[self.num_prompt_tokens:]
 
     @property
+    def num_cached_blocks(self):
+        return self.num_cached_tokens // self.block_size
+
+    @property
     def num_blocks(self):
         return (self.num_tokens + self.block_size - 1) // self.block_size
 
@@ -68,16 +75,15 @@ class Sequence:
         self.token_ids.append(token_id)
         self.last_token = token_id
         self.num_tokens += 1
+        self.prefilled = True
 
     def __getstate__(self):
-        last_state = self.last_token if not self.is_prefill else self.token_ids
-        return (self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.num_scheduled_tokens, self.block_table, last_state)
+        return (self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.block_table, self.prefilled,
+                self.last_token if self.prefilled else self.token_ids)
 
     def __setstate__(self, state):
-        self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.num_scheduled_tokens, self.block_table, last_state = state
-        if isinstance(last_state, list):
-            self.token_ids = last_state
-            self.last_token = self.token_ids[-1]
+        self.num_tokens, self.num_prompt_tokens, self.num_cached_tokens, self.block_table, self.prefilled = state[:-1]
+        if self.prefilled:
+            self.last_token = state[-1]
         else:
-            self.token_ids = []
-            self.last_token = last_state
+            self.token_ids = state[-1]
